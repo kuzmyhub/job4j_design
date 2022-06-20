e2f86185a4d76559d22c2183c177ae34e1a7bb29
Kuznetsov S. I <kuznetsov.s.i@bk.ru>
2022-06-20 20:39:52 +0300

    Временный

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,7 +1,8 @@
 package ru.job4j.map;
 
+import java.util.Date;
+import java.util.*;
 import java.util.Calendar;
-import java.util.Objects;
 
 public class User {
 
@@ -43,20 +44,25 @@ public class User {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return children == user.children
-                && Objects.equals(name, user.name)
-                && Objects.equals(birthday, user.birthday);
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(name, children, birthday);
+    }
+
+    public static void main(String[] args) {
+        User user1 = new User("Ivan", 1, new GregorianCalendar(2020, 0, 25, 10, 10, 10));
+        User user2 = new User("Ivan", 1, new GregorianCalendar(2020, 0, 25, 10, 10, 10));
+        System.out.println(user1.hashCode());
+        System.out.println(user2.hashCode());
+        HashMap<User, Object> map = new HashMap<>();
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        System.out.println(map);
     }
 }
