8b750fce3313605c315ea48a5ff15a2146d10eaf
Kuznetsov. S. I <kuznetsov.s.i@bk.ru>
2022-06-28 09:19:58 +0300

    2. Статистика по коллекции. [#45889]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -9,27 +9,42 @@ import java.util.stream.Stream;
 public class Analize {
 
     public static Info diff(Set<User> previous, Set<User> current) {
-        Info rsl = new Info(0, 0, 0);
-        Map<Integer, String>  prev = new HashMap<Integer, String>();
-        Map<Integer, String>  cur = new HashMap<Integer, String>();
-        prev = previous.stream()
+        Info rsl;
+        Map<Integer, String>  prevMap = previous.stream()
                 .collect(Collectors
                         .toMap(User::getId, User::getName)
                 );
-        cur = current.stream()
+        Map<Integer, String>  curMap = current.stream()
                 .collect(Collectors
                         .toMap(User::getId, User::getName)
                 );
-        for (Integer i : cur.keySet()) {
-            if (!prev.containsKey(i)) {
+        int prevSize = prevMap.size();
+        int curSize = curMap.size();
+
+        if (previous.equals(current)) {
+            rsl = new Info(0, 0, 0);
+        } else {
+            Map<Integer, String> valueMap = prevMap;
+            prevMap.remove(current.stream()
+                    .map(x -> x.getId())
+                    .collect(Collectors.toSet())); // удалённые в current
+            curMap.remove(current.stream()
+                    .map(x -> x.getId())
+                    .collect(Collectors.toSet())); // добавленные в currnet
+
+        }
+
+        for (Integer i : curMap.keySet()) {
+            if (!prevMap.containsKey(i)) {
                 rsl.setAdded(rsl.getAdded() + 1);
             }
-            if (!cur.get(i).equals(prev.get(i)) && prev.containsKey(i)) {
+            if (!curMap.get(i).equals(prevMap.get(i))
+                    && prevMap.containsKey(i)) {
                 rsl.setChanged(rsl.getChanged() + 1);
             }
         }
-        for (Integer j : prev.keySet()) {
-            if (!cur.containsKey(j)) {
+        for (Integer j : prevMap.keySet()) {
+            if (!curMap.containsKey(j)) {
                 rsl.setDeleted(rsl.getDeleted() + 1);
             }
         }
