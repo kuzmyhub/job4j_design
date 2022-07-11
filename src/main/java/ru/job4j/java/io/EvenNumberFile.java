57c4d64de131bad16775cd570081300206de8c72
Kuznetsov. S. I <kuznetsov.s.i@bk.ru>
2022-07-11 11:18:26 +0300

    0.2. FileInputStream [#4898]

--- src/main/java/ru/job4j/java/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/java/io/EvenNumberFile.java
@@ -1,7 +1,24 @@
 package ru.job4j.java.io;
 
+import com.sun.source.tree.UsesTree;
+
+import java.io.FileInputStream;
+
 public class EvenNumberFile {
     public static void main(String[] args) {
-
+        try (FileInputStream in = new FileInputStream("even.txt")) {
+            StringBuilder sb = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                sb.append((char) read);
+            }
+            String strNum = sb.toString();
+            String[] strArray = strNum.split(System.lineSeparator());
+            for (int i = 0; i < strArray.length; i++) {
+                System.out.println((Integer.parseInt(strArray[i]) % 2) == 0);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
