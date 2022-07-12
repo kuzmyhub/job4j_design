bcc4b1e9dda4b2c4c2bf5a0e9c4dd09155a4c2ff
Kuznetsov. S. I <kuznetsov.s.i@bk.ru>
2022-07-12 09:17:58 +0300

    1. Читаем файл конфигурации [#858]

--- src/main/java/ru/job4j/io/Config.java
+++ src/main/java/ru/job4j/io/Config.java
@@ -17,16 +17,44 @@ public class Config {
     }
 
     public void load() {
+        try (BufferedReader in = new BufferedReader(
+                new FileReader(this.path)
+        )) {
+            String[] strArray;
+            String read;
+            while ((read = in.readLine()) != null) {
+                 if (read.length() > 0
+                         && (read.indexOf("#") != 0)) {
+                    strArray = read.split("=", 2);
+                    if (!read.contains("=")
+                            || strArray.length < 2
+                            || read.indexOf("=") == 0
+                            || read.indexOf("=") == read.length() - 1) {
+                        throw new IllegalArgumentException(
+                                "Does not match the template \"key=value\""
+                        );
+                    }
+                    values.put(strArray[0], strArray[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(values);
     }
 
     public String value(String key) {
-        throw new UnsupportedOperationException("Don't impl this method yet!");
+        throw new UnsupportedOperationException(
+                "Don't impl this method yet!"
+        );
     }
 
     @Override
     public String toString() {
         StringJoiner out = new StringJoiner(System.lineSeparator());
-        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+        try (BufferedReader read = new BufferedReader(
+                new FileReader(this.path)
+        )) {
             read.lines().forEach(out::add);
         } catch (IOException e) {
             e.printStackTrace();
@@ -36,5 +64,7 @@ public class Config {
 
     public static void main(String[] args) {
         System.out.println(new Config("app.properties"));
+        Config conf = new Config("app.properties");
+        conf.load();
     }
 }
--- src/test/java/ru/job4j/io/ConfigTest.java
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -8,12 +8,40 @@ import static org.junit.Assert.assertThat;
 
 public class ConfigTest {
 
-    @Test
+    @Test (expected = IllegalArgumentException.class)
     public void whenPairWithoutComment() {
         String path = "./data/pair_without_comment.properties";
         Config config = new Config(path);
         config.load();
         assertThat(config.value("name"), is("Petr Arsentev"));
         assertThat(config.value("surname"), is(Matchers.nullValue()));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenPairWithoutKey() {
+        String path = "./data/pair_without_key.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenPairWithoutValue() {
+        String path = "./data/pair_without_value.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenPairWithoutKeyAndValue() {
+        String path = "./data/pair_without_key_and_value.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenPairWithoutSeparator() {
+        String path = "./data/pair_without_separator.properties";
+        Config config = new Config(path);
+        config.load();
     }
 }
