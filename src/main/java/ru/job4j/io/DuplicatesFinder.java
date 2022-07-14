7a27853ac903460c4b9304b8c329f264e0b32f0b
Kuznetsov. S. I <kuznetsov.s.i@bk.ru>
2022-07-14 09:51:45 +0300

    4.2. Поиск дубликатов [#315066]

---
+++ src/main/java/ru/job4j/io/DuplicatesFinder.java
@@ -0,0 +1,26 @@
+package ru.job4j.io;
+
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.io.IOException;
+import java.sql.SQLOutput;
+import java.util.List;
+import java.util.Map;
+
+public class DuplicatesFinder {
+    public static void main(String[] args) throws IOException {
+        DuplicatesVisitor duplicatesVisitor = new DuplicatesVisitor();
+        Files.walkFileTree(Path.of("."), duplicatesVisitor);
+        Map<FileProperty, List<Path>> map = duplicatesVisitor.getFiles();
+        for (FileProperty f : map.keySet()) {
+            List<Path> list = map.get(f);
+            System.out.println(f.getName()
+                    + " - "
+                    + f.getSize());
+            for (Path p : list) {
+                System.out.println(p);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/io/DuplicatesVisitor.java
@@ -0,0 +1,48 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.List;
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
+
+public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+
+    private List<FileProperty> list = new ArrayList<>();
+
+    private Set<FileProperty> setDup = new HashSet<>();
+
+    private Set<FileProperty> setAllValues = new HashSet<>();
+
+    private Map<FileProperty, List<Path>> map = new HashMap<>();
+
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        FileProperty fileProperty = new FileProperty(Files.size(file), file.getFileName().toString());
+        list.add(fileProperty);
+        setAllValues.add(fileProperty);
+        map.putIfAbsent(fileProperty, new ArrayList<>());
+        map.get(fileProperty).add(file);
+        return FileVisitResult.CONTINUE;
+    }
+
+    public Map<FileProperty, List<Path>> getFiles() {
+        for (FileProperty f : setAllValues) {
+            list.remove(f);
+        }
+        setDup.addAll(list);
+        setAllValues.removeAll(setDup);
+        for (FileProperty f : setAllValues) {
+            map.remove(f);
+        }
+        return map;
+    }
+}
