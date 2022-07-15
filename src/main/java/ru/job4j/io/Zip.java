c1c2b470873e76acb9f8fd524c02794d2e295288
Kuznetsov. S. I <kuznetsov.s.i@bk.ru>
2022-07-15 10:00:39 +0300

    5.2. Архивировать проект [#861]

---
+++ src/main/java/ru/job4j/io/Zip.java
@@ -0,0 +1,52 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+import java.nio.file.Path;
+
+public class Zip {
+
+    public void packFiles(List<Path> sources, File target) {
+
+    }
+
+    public void packSingleFile(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(
+                new BufferedOutputStream(
+                        new FileOutputStream(target)
+                ))) {
+            zip.putNextEntry(new ZipEntry(source.getPath()));
+            try (BufferedInputStream out = new BufferedInputStream(
+                    new FileInputStream(source)
+            )) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Zip zip = new Zip();
+        zip.packSingleFile(
+                new File("./pom.xml"),
+                new File("./pom.zip")
+        );
+        ArgsName argsName = ArgsName.of(args);
+        String[] argsArr = {
+                argsName.get("d"),
+                argsName.get("e"),
+                argsName.get("o")
+        };
+        Search.validation(argsArr);
+        Path path = Paths.get(argsName.get("d"));
+        List<Path> list = Search.search(path,
+                x -> x.toFile()
+                        .getName()
+                        .endsWith(argsName.get("e")));
+        zip.packFiles(list, new File(argsName.get("o")));
+    }
+}
