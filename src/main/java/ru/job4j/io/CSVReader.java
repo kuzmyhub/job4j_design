f214738474db144896bca6f5d86bb9917539f214
Sergey <kuznetsov.s.i@bk.ru>
2022-07-16 14:18:36 +0300

    7. Scanner [#504791]

---
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -0,0 +1,16 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringJoiner;
+import java.io.File;
+
+public class CSVReader {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("./CSVreader/table.csv");
+        Scanner scanner = new Scanner(file);
+        String[] str;
+        str =
+    }
+}
