502b08d37421c6550637922388d86d0fdfed1dfc
Sergey <kuznetsov.s.i@bk.ru>
2022-07-19 19:40:47 +0300

    4. Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -5,9 +5,14 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.IOException;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
+
+    public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
@@ -32,6 +37,8 @@ public class EchoServer {
                     out.flush();
                 }
             }
+        } catch (IOException e) {
+            LOG.error("IOException", e);
         }
     }
 }
--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,17 +8,10 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        byte b = 1;
-        short s = 2;
-        int i = 3;
-        long l = 4L;
-        float f = 0.5F;
-        double d = 0.6D;
-        char c = 'c';
-        boolean isTrue = true;
-        LOG.debug(
-                "Primitive types: {}, {}, {}, {}, {}, {}, {}, {}.",
-                b, s, i, l, f, d, c, isTrue
-        );
+        try {
+            throw new Exception("Not supported code");
+        } catch (Exception e) {
+            LOG.error("Exception in log example", e);
+        }
     }
 }
